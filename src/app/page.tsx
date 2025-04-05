d0d2996c9fa40bef3065e055f11d28473b7b4636
juniorcintra <junior.cintra.developer@gmail.com>
2025-04-05 13:50:51 -0300

    versão final tutorial 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,29 @@
-'use client'
+"use client";
 
-import SwitcherTheme from "@/components/SwitcherTheme"
-import { useTheme } from "next-themes"
-
+import SwitcherTheme from "@/components/SwitcherTheme";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const { theme } = useTheme()
+  const { theme } = useTheme();
+
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
 
   return (
-    <main className='h-screen bg-background flex justify-center items-center'>
+    <main className="bg-background flex h-screen items-center justify-center">
       <div className="flex flex-col items-center justify-center gap-4">
         <span className="text-foreground">Escolha um tema: </span>
         <SwitcherTheme />
-        <span className="text-foreground">Tema escolhido: {theme === "light" ? "Light" : "Dark"}</span>
+        <span className="text-foreground">
+          Tema escolhido: {theme === "light" ? "Light" : "Dark"}
+        </span>
       </div>
     </main>
-  )
+  );
 }
